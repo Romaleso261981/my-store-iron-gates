de00eaa8c1e4736fb3de5bd9ed15252d503a731f
Roman lesyo <ladiginscormag@gmail.com>
2024-02-06 13:45:11 +0200

    fix: added Spiner

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -2,15 +2,12 @@ import { Container } from '@mantine/core';
 
 import { FooterLinks } from '../components/Footer/Footer';
 import { HeaderTabs } from '../components/Header/Header';
-import { Welcome } from '../components/Welcome/Welcome';
 
 export function HomePage() {
   return (
     <>
       <HeaderTabs />
-      <Container maw={1280} p={0}>
-        <Welcome />
-      </Container>
+      <Container maw={1280} p={0}></Container>
       <FooterLinks />
     </>
   );
