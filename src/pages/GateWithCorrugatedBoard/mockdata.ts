755142cee6b9f8a567874544c3736e173ff0ba1c
Roman lesyo <ladiginscormag@gmail.com>
2024-02-09 17:47:47 +0200

    feat: Додав ще декілька воріт в базу

--- src/pages/GateWithCorrugatedBoard/mockdata.ts
+++ src/pages/GateWithCorrugatedBoard/mockdata.ts
@@ -62,5 +62,101 @@ export const mockdata = [
     code: 'B-01137',
     cost: '27 710',
     date: 'September 12, 2022'
+  },
+  {
+    id: '9',
+    title: 'ВОРОТА КОВАНІ З ПРОФНАСТИЛОМ І ХВІРТКОЮ',
+    image: 'https://images.prom.ua/3408321299_vorota-kovani-z.jpg',
+    code: 'B-01198',
+    cost: '24 800',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '10',
+    title: 'ВОРОТА КОВАНІ З ПРОФНАСТИЛОМ І ХВІРТКОЮ КОД: А-01248',
+    image: 'https://images.prom.ua/4351872141_vorota-kovani-z.jpg',
+    code: 'B-01248',
+    cost: '24 600',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '11',
+    title: 'КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ ТА КАЛІТКОЮ',
+    image: 'https://images.prom.ua/4407371527_kovani-vorota-z.jpg',
+    code: 'B-0216',
+    cost: '29 700',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '12',
+    title: 'КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ КОД: А-0107',
+    image: 'https://images.prom.ua/4407411811_kovani-vorota-z.jpg',
+    code: 'B-0107',
+    cost: '20 200',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '13',
+    title: 'КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ',
+    image: 'https://images.prom.ua/5077844271_kovani-vorota-z.jpg',
+    code: 'B-0174',
+    cost: '26 100',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '14',
+    title: 'ВОРОТА КОВАНІ З ПРОФНАСТИЛОМ І ХВІРТКОЮ КОД: А-01228',
+    image: 'https://images.prom.ua/4351854401_vorota-kovani-z.jpg',
+    code: 'B-01228',
+    cost: '24 600',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '15',
+    title: 'КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ',
+    image: 'https://images.prom.ua/4078555779_kovani-vorota-z.jpg',
+    code: 'B-0109',
+    cost: '20 300',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '16',
+    title: 'КОВАНІ РОЗПАШІ ВОРОТА З ПРОФНАСТИЛОМ',
+    image: 'https://images.prom.ua/4351840089_kovani-rozpashi-vorota.jpg',
+    code: 'B-0148',
+    cost: '28 400',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '17',
+    title: 'ЕКСКЛЮЗИВНІ КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ ТА КОВАНИМИ ЕЛЕМЕНТАМИ',
+    image: 'https://images.prom.ua/2234283777_eksklyuzivni-kovani-vorota.jpg',
+    code: 'B-0128',
+    cost: '27 600',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '18',
+    title: 'ВОРОТА КОВАНІ З ПРОФНАСТИЛОМ КОД: А-01201',
+    image: 'https://images.prom.ua/4684256626_vorota-kovani-z.jpg',
+    code: 'B-01201',
+    cost: '24 800',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '19',
+    title: 'ВОРОТА КОВАНІ З ПРОФНАСТИЛОМ І ХВІРТКОЮ',
+    image: 'https://images.prom.ua/4990090484_vorota-kovani-z.jpg',
+    code: 'B-01279',
+    cost: '18 150',
+    date: 'September 12, 2022'
+  },
+  {
+    id: '20',
+    title: 'КОВАНІ ВОРОТА З ПРОФНАСТИЛОМ ТА ЕЛЕМЕНТАМИ КОВКИ',
+    image: 'https://images.prom.ua/5077792165_kovani-vorota-z.jpg',
+    code: 'B-01100',
+    cost: '23 205',
+    date: 'September 12, 2022'
   }
 ];
