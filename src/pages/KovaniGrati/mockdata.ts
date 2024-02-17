260d61a2685cf0c3adb1af7d4a4afc3003d84467
Roman lesyo <ladiginscormag@gmail.com>
2024-02-17 09:47:48 +0200

    feat: added gratu

--- src/pages/KovaniGrati/mockdata.ts
+++ src/pages/KovaniGrati/mockdata.ts
@@ -7,5 +7,212 @@ export const mockdata = [
     cost: 1,
     category: 'kovaniGrati',
     date: 'August 18, 2022'
+  },
+  {
+    id: '2',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736871197_kovani-grati.jpg',
+    code: '0102',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '3',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736877862_kovani-grati.jpg',
+    code: '0103',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '4',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736880718_kovani-grati.jpg',
+    code: '0104',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '5',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736883082_kovani-grati.jpg',
+    code: '0105',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '6',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736885402_kovani-grati.jpg',
+    code: '0106',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '7',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736887538_kovani-grati.jpg',
+    code: '0107',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '8',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736889375_kovani-virobi.jpg',
+    code: '0108',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '9',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736891147_kovani-grati.jpg',
+    code: '0109',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '10',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736892894_kovani-grati.jpg',
+    code: '0110',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '11',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736895229_kovani-grati.jpg',
+    code: '0111',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '12',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736897176_kovani-grati.jpg',
+    code: '0112',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '13',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736899162_kovani-grati.jpg',
+    code: '0113',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '14',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736901109_kovani-grati.jpg',
+    code: '0114',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '15',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736902795_kovani-grati.jpg',
+    code: '0115',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '16',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736904266_kovani-grati.jpg',
+    code: '0116',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '17',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736906047_kovani-grati.jpg',
+    code: '0117',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '18',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736907488_kovani-grati.jpg',
+    code: '0118',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '19',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736909316_kovani-grati.jpg',
+    code: '0119',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '20',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736912073_kovani-grati.jpg',
+    code: '0121',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '21',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736913895_kovani-grati.jpg',
+    code: '0122',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '22',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736914894_kovani-grati.jpg',
+    code: '0123',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '23',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1865225506_kovani-grati.jpg',
+    code: '0128',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
+  },
+  {
+    id: '24',
+    title: 'КОВАНІ ГРАТИ',
+    image: 'https://images.prom.ua/1736917428_kovani-grati.jpg',
+    code: '0126',
+    cost: 1,
+    category: 'kovaniGrati',
+    date: 'August 18, 2022'
   }
 ];
